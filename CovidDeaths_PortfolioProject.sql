b4065185c064a96629cd4b5a49df0a05187f6a9d
MunibaSQL <munibapp@gmail.com>
2025-12-28 19:10:04 +0530

    Update CovidDeaths_PortfolioProject.sql

--- CovidDeaths_PortfolioProject.sql
+++ CovidDeaths_PortfolioProject.sql
@@ -1,5 +1,5 @@
 /*
-Covid 19 Data Exploration 
+Data Exploration 
 
 Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
 
@@ -149,4 +149,5 @@ Join PortfolioProject..CovidVaccinations vac
 	On dea.location = vac.location
 	and dea.date = vac.date
 where dea.continent is not null 
+
 
